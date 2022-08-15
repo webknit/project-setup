bb5f9d8ad50523cee9e2db6641d3669af8e73ec5
Shane Prendergast <shane.prendergast@sigma.se>
2022-08-15 10:19:23 +0100

    Update index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,33 +2,14 @@ import React from 'react';
 import './button.css';
 
 interface ButtonProps {
-    /**
-     * Is this the principal call to action on the page?
-     */
     primary?: boolean;
-    /**
-     * What background color to use
-     */
     backgroundColor?: string;
-    /**
-     * How large should the button be?
-     */
     size?: 'small' | 'medium' | 'large';
-    /**
-     * Button contents
-     */
     label: string;
-    /**
-     * Optional click handler
-     */
     onClick?: () => void;
-
-    testId: string;
+    testId?: string;
 }
 
-/**
- * Primary UI component for user interaction
- */
 const Button = ({ primary = false, size = 'medium', backgroundColor, label, testId, ...props }: ButtonProps) => {
     const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
     console.log(mode);
